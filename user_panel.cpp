ea4cd8d7cff4fe6596204fca3961038686f15333
SuperMonte <monah445@gmail.com>
2026-06-17 13:05:44 +0200

    change user ui

--- user_panel.cpp
+++ user_panel.cpp
@@ -10,15 +10,10 @@ user_panel::user_panel(QWidget *parent) :
     ui(new Ui::user_panel)
 {
     ui->setupUi(this);
-    // connect(ui->spin,SIGNAL(valueChanged(double)),this,SLOT(indicate(double)));
+    connect(ui->spin,SIGNAL(valueChanged(double)),this,SLOT(indicate(double)));
     connect(this,SIGNAL(enter_event(QObject*)),this,SLOT(key_catcher(QObject*)));
-//    tmr=new QTimer();
-//    tmr->setInterval(1300);
-//    connect(tmr,SIGNAL(timeout()),this,SLOT(auto_telemetry_call()));
-//    tmr->start();
     connect(this,SIGNAL(command_profed()),this,SLOT(data_received_and_profed()));
-    // ui->spin->installEventFilter(this);
-//    ID=7;
+    ui->spin->installEventFilter(this);
 }
 user_panel::~user_panel()
 {
@@ -31,21 +26,20 @@ void user_panel::key_catcher(QObject* key)
                                         "Send command on dc "+QString::number(ID)+"?",
                                         QMessageBox::Yes | QMessageBox::No);
     if(mesg->exec()==QMessageBox::Yes){
-//         if(key->objectName() == "spin"){
-//             QString message ="t";
-//             message.append(internal_address);
-//             message.append("811");
-//             message.append("00");
-//             message.append(QString("%1").arg(ID, 2, 16, QLatin1Char( '0' )));
-//             message.append("00");
-//             int value=ui->spin->value()*100;
-//             unsigned char *bytes = (unsigned char *)&value;
-//             unsigned char letters[] = {bytes[3],bytes[2],bytes[1],bytes[0]};
-//             QByteArray data=QByteArray(reinterpret_cast<char*>(letters),4);
-//             message.append(QString("%1").arg(value, 8, 16, QLatin1Char( '0' )));
-//             emit send_command(message.toUtf8()+'\r');
-// //            emit send_command(CURRENT_LASER,ID,QString::number(ui->spin->value()*10).replace(",","."));
-//         }
+        if(key->objectName() == "spin"){
+            QString message ="t";
+            message.append(internal_address);
+            message.append("811");
+            message.append("00");
+            message.append(QString("%1").arg(ID, 2, 16, QLatin1Char( '0' )));
+            message.append("00");
+            int value=ui->spin->value()*100;
+            unsigned char *bytes = (unsigned char *)&value;
+            unsigned char letters[] = {bytes[3],bytes[2],bytes[1],bytes[0]};
+            QByteArray data=QByteArray(reinterpret_cast<char*>(letters),4);
+            message.append(QString("%1").arg(value, 8, 16, QLatin1Char( '0' )));
+            emit send_command(message.toUtf8()+'\r');
+        }
     }
 }
 
@@ -62,7 +56,7 @@ void user_panel::data_received_and_profed()
         count_no_responce=0;
         enable_widget(true);
         if(raw_params[0].mid(5,2)=="91"){
-            ui->current_ld_label->setText(QString::number(nHex/100.0,'d',2)+" %");
+            ui->power_label->setText(QString::number(nHex/100.0,'d',2)+" %");
         }else if(raw_params[0].mid(5,2)=="90"){
             ui->power_state_label->setText(nHex?"ON":"OFF");
             ui->on_off_button->setChecked(nHex);
@@ -80,7 +74,7 @@ void user_panel::data_received_and_profed()
 
 void user_panel::indicate(double count)
 {
-    // ui->indicator->setValue(int(count*10)-100);
+    ui->indicator->setValue(int(count*10)-100);
 }
 
 void user_panel::on_on_off_button_clicked(bool checked)
